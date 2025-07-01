07864a83dc9fbbde062d1932895e9a7fc63b6191
william wilson <legendarywilson7@gmail.com>
2025-07-01 09:02:42 -0500

    drum-machine

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 // Setup 'tick' sound
-const tick = new Audio('sounds/tick.mp3');
+const tick = new Audio('sounds/tick.mp3');let count = 1;
+setInterval(() => {
+  if (count === 4) {
+    new Audio('tock.mp3').play();
+  } else {
+    new Audio('tick.mp3').play();
+  }
+  console.log(count); // debug output
+  count = (count % 4) + 1;
+}, 600);
+
+
+
 
 // This function is called every 600ms
 function update() {
